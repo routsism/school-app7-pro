46ecc53582648564095dc11bd5c2a856d91cbdb6
Routsis Michail <mike7199@icloud.com>
2025-03-05 20:48:24 +0200

    updates

---
+++ src/main/java/gr/aueb/cf/schoolapp/dao/ICItyDAO.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.schoolapp.dao;
+
+import gr.aueb.cf.schoolapp.model.City;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface ICItyDAO {
+
+    // All CRUD methods need to be defined.
+
+    List<City> getAll() throws SQLException;
+}
