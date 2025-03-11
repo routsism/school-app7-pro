88f693495e523f2a12e9eb3206996355da8c694f
Routsis Michail <mike7199@icloud.com>
2025-03-11 19:57:46 +0200

    update

--- src/main/java/gr/aueb/cf/schoolapp/authentication/AuthenticationManager.java
+++ src/main/java/gr/aueb/cf/schoolapp/authentication/AuthenticationManager.java
@@ -1,4 +1,12 @@
 package gr.aueb.cf.schoolapp.authentication;
 
+import java.util.Arrays;
+
 public class AuthenticationManager {
+
+    private AuthenticationManager() {}
+
+    public static boolean authenticate(String username, char[] password) {
+        return username.matches("[aA]dmin") && Arrays.equals(password, "12345".toCharArray());
+    }
 }
